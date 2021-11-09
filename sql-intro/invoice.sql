b27d903db2759d01e56f351fb91dbab110dde6d3
Maya Comfort <mdcomfort@gmail.com>
2021-11-08 21:33:29 -0500

    invoice done

--- sql-intro/invoice.sql
+++ sql-intro/invoice.sql
@@ -5,3 +5,23 @@ SELECT MAX(total) FROM invoice;
 
 SELECT MIN(total) FROM invoice;
 
+SELECT * FROM invoice
+WHERE total > 5;
+
+SELECT COUNT(*) FROM invoice
+WHERE total < 5
+
+SELECT * FROM invoice
+WHERE billing_state IN ('CA', 'TZ', 'AZ');
+
+SELECT AVG(total) FROM invoice;
+
+SELECT SUM(total) FROM invoice;
+
+UPDATE invoice 
+WHERE invoice_id = 5
+SET total = 24;
+
+DELETE
+FROM invoice
+WHERE invoice_id = 1;
