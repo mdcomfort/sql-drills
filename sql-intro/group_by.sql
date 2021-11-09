7aaf3e5ea60a8f82d453e239b5884adc5ef026d4
Maya Comfort <mdcomfort@gmail.com>
2021-11-08 22:20:17 -0500

    group_by done

--- sql-intro/group_by.sql
+++ sql-intro/group_by.sql
@@ -4,3 +4,8 @@ GROUP BY billing_state;
 SELECT AVG(milliseconds), album_id FROM track
 GROUP BY album_id
 ORDER BY AVG(milliseconds);
+
+SELECT COUNT(*), artist_id 
+FROM album
+WHERE artist_id IN (8, 22)
+GROUP BY artist_id;
